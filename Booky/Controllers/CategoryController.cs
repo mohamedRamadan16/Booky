cd810c641343c55d5327fd035143cfe823d53a0b
mohamedRamadan16 <113842111+mohamedRamadan16@users.noreply.github.com>
2024-11-09 22:50:02 +0200

    Category CRUD

---
+++ Booky/Controllers/CategoryController.cs
@@ -0,0 +1,109 @@
+﻿using Booky.Data;
+using Booky.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Booky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext DbContext;
+        public CategoryController(ApplicationDbContext _DbContext)
+        {
+            DbContext = _DbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = DbContext.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        public IActionResult ConfirmCreate(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", category);
+            }
+
+            // custome validator
+            if (category.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is not allowed in category name");
+                return View("Create", category);
+            }
+
+
+            DbContext.Categories.Add(category);
+            DbContext.SaveChanges();
+            TempData["success"] = "Category Created Successfully";
+            return RedirectToAction("Index");   
+        }
+
+        public IActionResult Edit(int? Id)
+        {
+            if (Id == null || Id == 0)
+                return NotFound();
+
+            Category? category = DbContext.Categories.FirstOrDefault(c => c.Id == Id);
+            if (category == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ActionName("Edit")]
+        public IActionResult ConfirmEdit(Category category)
+        {
+            if (!ModelState.IsValid || category == null)
+            {
+                return View("Edit", category);
+            }
+
+            // custome validator
+            if (category.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is not allowed in category name");
+                return View("Edit", category);
+            }
+
+            DbContext.Categories.Update(category);
+            DbContext.SaveChanges();
+            TempData["success"] = "Category Updated Successfully";
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Delete(int Id)
+        {
+            Category? category = DbContext.Categories.FirstOrDefault(c => c.Id == Id);
+            if(category == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult ConfirmDelete(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                DbContext.Categories.Remove(category);
+                DbContext.SaveChanges();
+                TempData["success"] = "Category Deleted Successfully";
+                return RedirectToAction("Index");
+            }
+            return View("Delete" ,category);
+        }
+    }
+}
---
+++ Booky/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Booky.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Booky.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        public ApplicationDbContext() { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // seeding some categories.
+            modelBuilder.Entity<Category>().HasData(
+                new Category {Id = 1, Name="Action", DisplayOrder=1},
+                new Category {Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category {Id = 3, Name = "History", DisplayOrder = 3 }
+             );
+        }
+
+    }
+}

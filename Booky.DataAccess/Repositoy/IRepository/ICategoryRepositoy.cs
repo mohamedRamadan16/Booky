8943a8733a1f36a4e924105dc1568fdcfb4f601d
mohamedRamadan16 <113842111+mohamedRamadan16@users.noreply.github.com>
2024-11-12 01:00:57 +0200

    Adding Generic Repository

---
+++ Booky.DataAccess/Repositoy/IRepository/ICategoryRepositoy.cs
@@ -0,0 +1,15 @@
+﻿using Booky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booky.DataAccess.Repositoy.IRepository
+{
+    public interface ICategoryRepositoy:IRepository<Category>
+    {
+        void Update(Category obj);
+        int Save();
+    }
+}

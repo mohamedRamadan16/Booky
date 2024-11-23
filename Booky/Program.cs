aa8c26b048ac76e08d6e8f15135a4f8b011ee589
mohamedRamadan16 <113842111+mohamedRamadan16@users.noreply.github.com>
2024-11-23 09:54:42 +0200

    Adding Authentication And Authorization With Identity Scaffolding

--- Booky/Program.cs
+++ Booky/Program.cs
@@ -2,12 +2,16 @@ using Booky.DataAccess.Data;
 using Booky.DataAccess.Repositoy;
 using Booky.DataAccess.Repositoy.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
 
@@ -23,8 +27,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
